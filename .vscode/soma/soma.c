cf1f2ebb62de43be4ebf74547cdb169831814573
AdrianoFreitas32 <adriano.lfreitas324@gmail.com>
2025-03-27 22:21:32 +0000

    soma printf

---
+++ .vscode/soma/soma.c
@@ -0,0 +1,15 @@
+void main() {
+
+    int num;
+    printf("Etre com um inteiro:\n");
+    scanf("%d", &num);
+    num=num+num%3;
+    printf("num=%d\n", num);
+int n,i;
+i=20;
+n=2*(i%3);
+printf("n=%d",n);
+
+
+
+}
