2d89c00c52f5ed1a157e9d0b38fcfbe78b5b6df2
AdrianoFreitas32 <adriano.lfreitas324@gmail.com>
2025-03-27 18:43:28 +0000

    primeiroteste

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,8 @@
 //Teste larissa
 
 int main() {
+    
+    printf("carta super trunfo!!!\n");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -17,6 +19,27 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    int carta = 01;
+    char Estado = "MT";
+    char codigo = "MT01";
+    char cidade = "tangara da serra";
+    int populaçao = 150.000;
+    float Area = 1250;
+    
+    float PIB = 556.270;
+    int pontosturisticos = 5;
+    
+    int carta = 02;
+    char Estado[15] = "MT";
+    char codigo[15] = "MT02";
+    char cidade[15] = 'nova olimpia';
+    int populaçao = 25.000;
+    float Area = 250;
+    float PiB = 56.780;
+    int pontosturisticos = 8;
 
+    printf("codigo da carta %s é: %d\n", codigo);
+
+    
     return 0;
 }
